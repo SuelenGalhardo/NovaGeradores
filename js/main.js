b4e3d9f3bd7f33e303b4309098e7963a70d3a8ab
SuelenGalhardo <suelenranucci@gmail.com>
2024-07-15 17:15:01 +0200

    added scss sass

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const headerContainer = document.getElementById("header-container");
+    const headerContainer = document.getElementById("header");
 
     if (headerContainer) {
         fetch("../pages/header.html")
             .then(response => response.text())
             .then(data => {
                 headerContainer.innerHTML = data;
-            });
-    }
 
-    const menuLinks = document.querySelectorAll('nav ul li a');
-    menuLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-            if (targetSection) {
-                targetSection.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    });
+               
+                const menuLinks = document.querySelectorAll('.header__nav--a');
+                menuLinks.forEach(link => {
+                    link.addEventListener('click', function(event) {
+                        event.preventDefault();
+                        const targetId = this.getAttribute('href').substring(1);
+                        const targetSection = document.getElementById(targetId);
+                        if (targetSection) {
+                            targetSection.scrollIntoView({ behavior: 'smooth' });
+                        }
+                    });
+                });
+            })
+            .catch(error => console.error('Error al cargar el header:', error));
+    }
 });
